154d4c19dbf4112bb450176bd4742354ec834cc7
hobuttt <hobuttt.ne@gmail.com>
2018-01-29 15:33:30 +0300

    ver 0.0.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
 
 
-})
+  $(function() {
+    $('.nav__icon').on('click', function() {
+      $(this).closest('.nav__box').toggleClass('menu_state_open');
+    });
+  });
+
+
+
+});
